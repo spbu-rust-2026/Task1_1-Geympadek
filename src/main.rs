2731b6477f46b57b3b840ad83ddbb58177243d3f
Geympadek <andreltsov08@outlook.com>
2026-09-17 18:49:44 +0300

    changed splitting behaivour to whitespace and newline characters only

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,10 @@ fn main() {
     stdin().read_line(&mut input).expect("Unable to read input");
 
     let mut result: Int = 0;
-    for slice in input.split_whitespace() {
+    for slice in input.split(|c| c == ' ' || c == '\n' ) {
+        if slice.len() == 0 {
+            continue;
+        }
         result += slice.parse::<Int>().expect("Unable to parse given value");
     }
 
