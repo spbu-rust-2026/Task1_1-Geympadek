c4b307015fd5932c8b303608990db47cf539d9a6
Geympadek <andreltsov08@outlook.com>
2026-09-17 18:33:40 +0300

    changed type to i128

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use std::io::{stdin};
 
+type int = i128;
+
 fn main() {
     let mut input = String::new();
     stdin().read_line(&mut input).expect("Unable to read input");
 
-    let mut result = 0;
+    let mut result: int = 0;
     for slice in input.split_whitespace() {
-        result += slice.parse::<i32>().expect("Unable to parse given value");
+        result += slice.parse::<int>().expect("Unable to parse given value");
     }
 
     print!("{result}");
